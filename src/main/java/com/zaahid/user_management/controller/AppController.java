14268b7b938a34416f25d10b11927998abcce5e2
Muhammad Zaahid <muhammadzaahid24@gmail.com>
2021-11-13 17:05:42 +0700

    Home page

--- src/main/java/com/zaahid/user_management/controller/AppController.java
+++ src/main/java/com/zaahid/user_management/controller/AppController.java
@@ -1,5 +1,25 @@
 package com.zaahid.user_management.controller;
 
+import java.util.List;
+
+import com.zaahid.user_management.model.User;
+import com.zaahid.user_management.services.UserServices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class AppController {
     
+    @Autowired
+    UserServices services;
+
+    @RequestMapping("/")
+    public String HomePage(Model model){
+        List<User> listUser = services.listAll();
+        model.addAttribute("listUser", listUser);
+        return "index";
+    }
 }
