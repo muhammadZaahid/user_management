bf930bb855de9f62bcf0847172b65be920f0d288
Muhammad Zaahid <muhammadzaahid24@gmail.com>
2021-11-13 16:39:17 +0700

    set getter setter

--- src/main/java/com/zaahid/user_management/model/User.java
+++ src/main/java/com/zaahid/user_management/model/User.java
@@ -18,5 +18,30 @@ public class User {
     private String phoneNumber;
     private String email;
     
+    public Long getUid() {
+        return uid;
+    }
+    public void setUid(Long uid) {
+        this.uid = uid;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
 
 }
