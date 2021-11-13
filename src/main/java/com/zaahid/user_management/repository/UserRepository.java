2d4cce585f37897181c1c1a4174d9217f10ed884
Muhammad Zaahid <muhammadzaahid24@gmail.com>
2021-11-13 16:41:06 +0700

    config user repository file

--- src/main/java/com/zaahid/user_management/repository/UserRepository.java
+++ src/main/java/com/zaahid/user_management/repository/UserRepository.java
@@ -1,5 +1,11 @@
 package com.zaahid.user_management.repository;
 
-public class UserRepository {
+import com.zaahid.user_management.model.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
     
 }
