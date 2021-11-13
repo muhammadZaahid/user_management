91501a75fa928a16545792a89ee9971871a1feb9
Muhammad Zaahid <muhammadzaahid24@gmail.com>
2021-11-13 16:59:24 +0700

    config service

--- src/main/java/com/zaahid/user_management/services/UserServices.java
+++ src/main/java/com/zaahid/user_management/services/UserServices.java
@@ -1,5 +1,32 @@
 package com.zaahid.user_management.services;
 
+import java.util.List;
+
+import com.zaahid.user_management.model.User;
+import com.zaahid.user_management.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserServices {
     
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> listAll(){
+        return repo.findAll();
+    }
+
+    public void save(User user ){
+        repo.save(user);
+    }
+
+    public User get(Long id){
+        return repo.findById(id).get();
+    }
+
+    public void delete(Long id){
+        repo.deleteById(id);
+    }
 }
