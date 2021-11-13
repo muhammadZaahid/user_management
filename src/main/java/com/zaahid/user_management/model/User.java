dcbfad8802d2955cbf61bb224f1deb5605ac25e8
Muhammad Zaahid <muhammadzaahid24@gmail.com>
2021-11-13 16:38:27 +0700

    set attribute

--- src/main/java/com/zaahid/user_management/model/User.java
+++ src/main/java/com/zaahid/user_management/model/User.java
@@ -1,6 +1,22 @@
 package com.zaahid.user_management.model;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "user")
+public class User {     
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long uid;
+    private String name;
+    private String phoneNumber;
+    private String email;
     
-    
+
 }
