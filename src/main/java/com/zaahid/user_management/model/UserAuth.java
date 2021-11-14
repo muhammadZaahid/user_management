adfe3fef317a63af09ca449d786b1de64a9fe06b
Muhammad Zaahid <muhammadzaahid24@gmail.com>
2021-11-14 21:11:33 +0700

    declare table users for auth, and table relation

--- src/main/java/com/zaahid/user_management/model/UserAuth.java
+++ src/main/java/com/zaahid/user_management/model/UserAuth.java
@@ -1,5 +1,35 @@
 package com.zaahid.user_management.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class UserAuth {
+    
+    @Id
+    @Column(name = "role_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private boolean enabled;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name= "user_id"), inverseJoinColumns = @JoinColumn(name ="role_id"))
+    private Set<Role> roles=new HashSet<>();
+
     
 }
