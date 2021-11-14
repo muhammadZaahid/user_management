e28d43373ad1cc7f093f9c35627ce49371f88978
Muhammad Zaahid <muhammadzaahid24@gmail.com>
2021-11-14 20:34:51 +0700

    Declare table, column, and attribute for role

--- src/main/java/com/zaahid/user_management/model/Role.java
+++ src/main/java/com/zaahid/user_management/model/Role.java
@@ -1,5 +1,20 @@
 package com.zaahid.user_management.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
 public class Role {
+
+    @Id
+    @Column(name = "role_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
     
 }
