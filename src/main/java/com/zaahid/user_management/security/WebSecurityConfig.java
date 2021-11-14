146660a9f4a6cf994e9f6a9585c03951d3b6fdc6
Muhammad Zaahid <muhammadzaahid24@gmail.com>
2021-11-14 23:43:25 +0700

    Add method for password encoder and auth provider

--- src/main/java/com/zaahid/user_management/security/WebSecurityConfig.java
+++ src/main/java/com/zaahid/user_management/security/WebSecurityConfig.java
@@ -1,7 +1,30 @@
 package com.zaahid.user_management.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+      return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+
+    }
     
 }
