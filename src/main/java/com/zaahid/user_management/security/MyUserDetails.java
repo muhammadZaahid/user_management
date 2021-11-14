2432ac393b8587ca7099d1b1c2f53e8ef5b019e5
Muhammad Zaahid <muhammadzaahid24@gmail.com>
2021-11-14 23:30:14 +0700

    Add method for get user role

---
+++ src/main/java/com/zaahid/user_management/security/MyUserDetails.java
@@ -0,0 +1,69 @@
+package com.zaahid.user_management.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import com.zaahid.user_management.model.Role;
+import com.zaahid.user_management.model.UserAuth;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class MyUserDetails implements UserDetails{
+
+    private UserAuth userAuth;
+    public MyUserDetails(UserAuth userAuth){
+        this.userAuth=userAuth;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        
+        Set<Role> roles=userAuth.getRoles();
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        for(Role role:roles){
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        
+        return false;
+    }
+    
+}
