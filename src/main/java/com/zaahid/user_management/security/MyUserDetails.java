43f72632fbfd1a4d26f41c292998dfea1df4c91f
Muhammad Zaahid <muhammadzaahid24@gmail.com>
2021-11-14 23:33:29 +0700

    config some method

--- src/main/java/com/zaahid/user_management/security/MyUserDetails.java
+++ src/main/java/com/zaahid/user_management/security/MyUserDetails.java
@@ -39,31 +39,31 @@ public class MyUserDetails implements UserDetails{
     @Override
     public String getUsername() {
         
-        return null;
+        return userAuth.getUsername();
     }
 
     @Override
     public boolean isAccountNonExpired() {
         
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
         
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
         
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
         
-        return false;
+        return userAuth.isEnabled();
     }
     
 }
