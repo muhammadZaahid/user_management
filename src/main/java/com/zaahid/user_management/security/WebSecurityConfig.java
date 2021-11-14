1b74de3dc0a776d77fce94eb71e6b43dfba978ee
Muhammad Zaahid <muhammadzaahid24@gmail.com>
2021-11-14 23:39:50 +0700

    Add web config file

---
+++ src/main/java/com/zaahid/user_management/security/WebSecurityConfig.java
@@ -0,0 +1,7 @@
+package com.zaahid.user_management.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+    
+}
